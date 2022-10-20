f4d749938ceb5691a786b2bc352ecb7a8e7a5782
CristianFGC12 <crifergon12@gmail.com>
2022-10-19 18:39:15 -0600

    Inicializar recursos anteriores

---
+++ EDII_LAB_05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDII_LAB_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
