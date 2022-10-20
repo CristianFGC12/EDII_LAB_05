ec1531a891a4d237c054e781ae4a0f9123bdee46
CristianFGC12 <crifergon12@gmail.com>
2022-10-19 19:03:49 -0600

    Cambios de los archivos iniciales

---
+++ EDII_LAB_05/Models/Compania.cs
@@ -0,0 +1,18 @@
+﻿using Huffman;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDII_LAB_05.Models
+{
+    public class Compania
+    {
+        public string Name { get; set; }
+        public byte[] dpicod { get; set; }
+        [JsonIgnore]
+        public HuffmanTree Libreria = new HuffmanTree();
+    }
+}
---
+++ EDII_LAB_05/Models/Solicitante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDII_LAB_05.Models
+{
+    public class Solicitante
+    {
+        public string name { get; set; }
+        public string dpi { get; set; }
+        public string dateBirth { get; set; }
+        public string address { get; set; }
+        public List<string> companies { get; set; }
+        public string recluiter { get; set; }
+    }
+}
